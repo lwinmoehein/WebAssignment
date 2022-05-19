38d49501840f67d95145dcbcb3cc80bd08b47c71
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-05-19 16:26:20 +0630

    add nav links style

--- resources/views/layouts/side-bar.blade.php
+++ resources/views/layouts/side-bar.blade.php
@@ -1,20 +1,20 @@
-<nav>
+<div>
 
-    <ul class="side-menu">
-        <li class="{{Route::is('students.index')|| Route::is('index')?'link-active':''}}">
+    <div class="side-menu">
+        <div class="{{Route::is('students.index')|| Route::is('index')?'link-active':''}}">
             <a href="{{route('students.index')}}" >
                 <i class="fa fa-film"></i>
                 <span>Student List</span>
             </a>
-        </li>
+        </div>
 
 
-        <li class="{{Route::is('students.create')?'link-active':''}}">
+        <div class="{{Route::is('students.create')?'link-active':''}}">
             <a href="{{route('students.create')}}">
                 <i class="fa fa-tv"></i>
                 <span>Student Register</span>
             </a>
-        </li>
+        </div>
 
-    </ul>
-</nav>
+    </div>
+</div>
